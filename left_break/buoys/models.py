b4df396a3ebc1b17fba48719703b4163313f9709
Sean Bleier <sebleier@gmail.com>
2010-08-14 00:21:10 -0700

    Added model for buoys.  Oh... and mint_xoltron and matt_c rockdahouse as well :)

--- left_break/buoys/models.py
+++ left_break/buoys/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Buoy(models.Model):
+    name = models.CharField(max_length=50)
+    point = models.PointField(srid=4326)
+
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return u"<Buoy %s" % self.name
