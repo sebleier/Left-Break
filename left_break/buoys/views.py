eb429c371b0bbf82f5de33737272181455c17a62
Sean Bleier <sebleier@gmail.com>
2010-08-14 17:26:35 -0700

    Added the json view for the buoys

--- left_break/buoys/views.py
+++ left_break/buoys/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from left_break.decorators import json_view
+from left_break.buoys.models import Buoy
+
+@json_view
+def buoys(request):
+    poly = Polygon((
+        (-170.0, 0.0),
+        (-170.0, 65.0),
+        (-114.0, 65.0),
+        (-114.0, 0.0),
+        (-170, 0.0)),
+        srid=4326,
+    )
+    buoys = Buoy.objects.filter(point__within=poly)
+    return [{'id': buoy.name, 'coords': buoy.point.coords}for buoy in buoys]
